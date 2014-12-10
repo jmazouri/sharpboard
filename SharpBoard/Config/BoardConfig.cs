a762c016b6048d224c38ba15ed9d1bda2d785107
Jmazouri <jmazouri@gmail.com>
2014-12-10 13:47:36 -0500

    Initial Configuration Improved starting template slightly

---
+++ SharpBoard/Config/BoardConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpBoard.Config
+{
+    public class BoardConfig
+    {
+
+    }
+}
