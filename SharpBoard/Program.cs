e8feccd6fc559a7284b3ed4b17940db8d044226f
John Mazouri <jmazouri@gmail.com>
2015-01-15 16:15:56 -0500

    Added post button to single board view Made URI sourced from environment variable, and added some validation for it

--- SharpBoard/Program.cs
+++ SharpBoard/Program.cs
@@ -9,15 +9,36 @@ namespace SharpBoard
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            using (var host = new NancyHost(new Uri("http://localhost:1234")))
+            string envUrl = Environment.GetEnvironmentVariable("SharpboardURL", EnvironmentVariableTarget.Machine);
+
+            if (envUrl == null)
+            {
+                Console.WriteLine("Error: System environment variable \"SharpboardURL\" not found! Make sure you set it at the machine level!");
+                return 203;
+            }
+
+            Uri hostUri = new Uri("http://localhost:80");
+
+            try
+            {
+                hostUri = new Uri(envUrl);
+            }
+            catch (UriFormatException)
+            {
+                Console.WriteLine("Error: URI \"{0}\" is malformed. Using default of \"{1}\"", envUrl, hostUri.OriginalString);
+                return 1;
+            }
+            
+            using (var host = new NancyHost(hostUri))
             {
                 Console.WriteLine("Starting Host...");
                 host.Start();
                 Console.WriteLine("Started!");
 
                 Console.ReadLine();
+                return 0;
             }
         }
     }
