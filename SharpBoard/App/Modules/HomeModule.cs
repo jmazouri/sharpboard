df818ce25e0afb0b3ce481f1c687adf7ad96b79b
John Mazouri <jmazouri@gmail.com>
2015-01-15 14:01:54 -0500

    Switching from Mercurial to Git Fixed some error handling

--- SharpBoard/App/Modules/HomeModule.cs
+++ SharpBoard/App/Modules/HomeModule.cs
@@ -8,7 +8,7 @@ namespace SharpBoard.App.Modules
     {
         public HomeModule()
         {
-            Get["/"] = _ => View["Home/Index"];
+            Get["/"] = _ => View["Home/Index", new {PartialTitle = "Home"}];
             Get["/error/{errorcode?}"] = _ =>
             {
                 string errorString = ErrorHandling.Codes[ErrorCode.Default];
@@ -18,7 +18,7 @@ namespace SharpBoard.App.Modules
                     errorString = ErrorHandling.Codes[Enum.Parse(typeof (ErrorCode), _.errorcode)];
                 }
 
-                return View["Shared/Error", errorString].WithStatusCode(HttpStatusCode.NotFound);
+                return View["Shared/Error", new {errorString, PartialTitle = "Error"}].WithStatusCode(HttpStatusCode.NotFound);
             };
         }
     }
