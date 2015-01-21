a6dc9998af23bafc2acf3c59032367942893818e
John Mazouri <jmazouri@gmail.com>
2015-01-20 20:24:50 -0500

    Implemented Topshelf for windows service integration

--- SharpBoard/App/Util/Util.cs
+++ SharpBoard/App/Util/Util.cs
@@ -13,5 +13,28 @@ namespace SharpBoard
         {
             return Enum.GetName(typeof (ErrorCode), code);
         }
+
+        public static Uri GetUri()
+        {
+            string envUrl = Environment.GetEnvironmentVariable("SharpboardURL", EnvironmentVariableTarget.Machine);
+
+            if (envUrl == null)
+            {
+                Console.WriteLine("Error: System environment variable \"SharpboardURL\" not found! Make sure you set it at the machine level!");
+            }
+
+            Uri hostUri = new Uri("http://localhost:80");
+
+            try
+            {
+                hostUri = new Uri(envUrl);
+            }
+            catch (UriFormatException)
+            {
+                Console.WriteLine("Error: URI \"{0}\" is malformed. Using default of \"{1}\"", envUrl, hostUri.OriginalString);
+            }
+
+            return hostUri;
+        }
     }
 }
--- SharpBoard/Program.cs
+++ SharpBoard/Program.cs
@@ -4,42 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Nancy.Hosting.Self;
+using Topshelf;
 
 namespace SharpBoard
 {
     class Program
     {
-        static int Main(string[] args)
+        static void Main(string[] args)
         {
-            string envUrl = Environment.GetEnvironmentVariable("SharpboardURL", EnvironmentVariableTarget.Machine);
-
-            if (envUrl == null)
+            HostFactory.Run(x =>
             {
-                Console.WriteLine("Error: System environment variable \"SharpboardURL\" not found! Make sure you set it at the machine level!");
-                return 203;
-            }
+                x.Service<NancyHost>(s =>
+                {
+                    s.ConstructUsing(name => new NancyHost(Util.GetUri()));
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
 
-            Uri hostUri = new Uri("http://localhost:80");
+                x.RunAsLocalSystem();
+                x.StartAutomatically();
 
-            try
-            {
-                hostUri = new Uri(envUrl);
-            }
-            catch (UriFormatException)
-            {
-                Console.WriteLine("Error: URI \"{0}\" is malformed. Using default of \"{1}\"", envUrl, hostUri.OriginalString);
-                return 1;
-            }
-            
-            using (var host = new NancyHost(hostUri))
-            {
-                Console.WriteLine("Starting Host...");
-                host.Start();
-                Console.WriteLine("Started!");
-
-                Console.ReadLine();
-                return 0;
-            }
+                
+                x.SetDescription("SharpBoard Image Board Server");
+                x.SetDisplayName("Sharpboard");
+                x.SetServiceName("Sharpboard");
+            });
         }
     }
 }
